bfe2d2c2662eae530784ac3035c7118558dd193b
JohanCelis148 <johan2004celis@gmail.com>
2023-03-27 16:41:42 -0500

    l

---
+++ logica/calcular.php
@@ -0,0 +1,34 @@
+<?php
+
+//Pregunta 1 : Boleano
+
+$precio=$_POST['precio'];
+$marca=$_POST['marca'];
+$iva=$_POST['iva'];
+$descuento= 0;
+$total = 0;
+$valorpagar = 0;
+
+
+if($precio >= 2000){
+    $descuento = $precio * 0.1;
+}
+
+if($marca = "LG"){
+    $descuento = $descuento + ($precio * 0.05);
+}
+
+$total= $precio - $descuento;
+$iva = ($total*$iva)/100;
+
+$valorpagar = $total + $iva;
+
+
+echo "<script>alert('El precio es: $precio')</script>";
+echo "<script>alert('El descuento aplicado es: $descuento')</script>";
+echo "<script>alert('El iva aplicado es: $iva')</script>";
+echo "<script>alert('El valor a pagar con iva es: $valorpagar')</script>";
+echo "<script>window.open('../index.html','_self')</script>";
+
+
+?>
